95c321550f18642050dacf1adb38af05f141c97a
WCFS <rjmrecycling@gmail.com>
2018-11-02 22:49:37 +0000

    Update verify.js

--- js/verify.js
+++ js/verify.js
@@ -1,5 +1,7 @@
 jQuery(function($) {
   $("#submit").on("click", function() {
-    alert("clicked");
+    $.getJSON('https://api.guildwars2.com/v2/guild/A76E8CA4-7596-E311-941E-AC162DAE5AD5/upgrades?access_token=8F1820AA-0D42-CB4F-870F-01C32BE956EF5135E5D6-1AFE-46AA-BB49-14EC8B3D65B3', function(data) {
+      console.log(data);
+    });
   });
 });
