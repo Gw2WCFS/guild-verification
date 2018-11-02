0bcd5af3cba885ae45b658ef350f9e03101e3aad
WCFS <rjmrecycling@gmail.com>
2018-11-02 22:57:32 +0000

    Update verify.js

--- js/verify.js
+++ js/verify.js
@@ -1,7 +1,7 @@
 jQuery(function($) {
   $("#submit").on("click", function() {
     $.getJSON('https://api.guildwars2.com/v2/guild/A76E8CA4-7596-E311-941E-AC162DAE5AD5', function(data) {
-      console.log(data);
+      console.log(data.name);
     });
   });
 });
